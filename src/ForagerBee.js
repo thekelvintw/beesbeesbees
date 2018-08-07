da68f81a8b43f1dd4681772a7c3a5d66e1cb20ae
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-07 13:31:50 -0700

    finished all specs

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,10 +1,17 @@
 var ForagerBee = function() {
-	Bee(this);
+	Grub.call(this);
+	Bee.call(this);
 	this.age = 10;
 	this.job = 'find pollen';
-
+	this.color ='yellow';
+	this.canFly = true;
+	this.treasureChest = [];
 };
 
 
-ForagerBee.prototype = Object.create(Beee);
+ForagerBee.prototype = Object.create(Grub.prototype);
 ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage =function(){
+	this.treasureChest.push('treasure');
+}
