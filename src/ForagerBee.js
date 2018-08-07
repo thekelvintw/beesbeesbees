672fc6aeff10016299e1761a0b871037e154343a
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-07 00:03:19 -0700

    finished HoneyMakerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,10 @@
 var ForagerBee = function() {
+	Bee(this);
+	this.age = 10;
+	this.job = 'find pollen';
+
 };
 
+
+ForagerBee.prototype = Object.create(Beee);
+ForagerBee.prototype.constructor = ForagerBee;
